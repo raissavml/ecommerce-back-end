ea1f77a52fd48b53ea2ccd929cb7b8a54b70db64
Raíssa <raissavml@hotmail.com>
2023-05-27 22:05:35 -0300

    ativação da rota de produtos

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import app from './src/app.js'
 import * as dotenv from 'dotenv'
 import dbConnect from './src/database/database.js'
-import routes  from './src/routes/userRoutes.js'
+import routesUsers  from './src/routes/userRoutes.js'
+import routesProducts  from './src/routes/productsRoutes.js'
 
 dotenv.config()
 dbConnect()
 
-app.use(routes)
+app.use(routesUsers)
+app.use(routesProducts)
 
 
 const PORT = process.env.PORT
--- src/controllers/ProductControllers.js
+++ src/controllers/ProductControllers.js
@@ -1,8 +1,8 @@
-import UserSchema from '../models/productSchema.js'
+import ProductSchema from '../models/productSchema.js'
 
 const postProduct = async (req,res) => {
     try{
-        const newProduct = new UserSchema(req.body)
+        const newProduct = new ProductSchema(req.body)
         const saveProduct = await newProduct.save()
         res.status(201).json({msg: saveProduct})
     }catch(e) {
@@ -13,7 +13,7 @@ const postProduct = async (req,res) => {
 const getProductbyId = async (req,res) => {
     try {
         const { id } = req.params
-        const productById = await UserSchema.findById({_id: id})
+        const productById = await ProductSchema.findById({_id: id})
         res.status(200)
         return res.json(productById)
     } catch(e) {
@@ -24,7 +24,7 @@ const getProductbyId = async (req,res) => {
 const getProductbySize = async (req,res) => {
     try{
         const {size} = req.query.size
-        const products = await UserSchema.find({size})
+        const products = await ProductSchema.find({size})
         return res.status(200).json(products)
    }catch(e) {
         return res.status(400).json({msg: e.message})
@@ -34,7 +34,7 @@ const getProductbySize = async (req,res) => {
 const getProductbyColor = async (req,res) => {
     try{
         const {color} = req.query.color
-        const products = await UserSchema.find({color})
+        const products = await ProductSchema.find({color})
         return res.status(200).json(products)
    }catch(e) {
         return res.status(404).json({msg: e.message})
@@ -44,7 +44,7 @@ const getProductbyColor = async (req,res) => {
 const getProductbyPrice = async (req,res) => {
     try{
         const {price} = req.query.price
-        const products = await UserSchema.find({price})
+        const products = await ProductSchema.find({price})
         return res.status(200).json(products)
    }catch(e) {
         return res.status(404).json({msg: e.message})
@@ -53,7 +53,7 @@ const getProductbyPrice = async (req,res) => {
 
 const getAllProducts = async (req,res) => {
    try{
-        const products = await UserSchema.find()
+        const products = await ProductSchema.find()
         return res.status(200).json(products)
    }catch(e) {
         return res.status(404).json({msg: e.message})
@@ -64,7 +64,7 @@ const getAllProducts = async (req,res) => {
 const updateProductByID = async (req,res) => {
     try{
         const { id } = req.params
-        const updateProduct = await UserSchema.findByIdAndUpdate({_id: id}, req.body)
+        const updateProduct = await ProductSchema.findByIdAndUpdate({_id: id}, req.body)
         return res.status(200).json({msg: `Product updated`, data: {
             updateProduct
         }})
@@ -76,7 +76,7 @@ const updateProductByID = async (req,res) => {
 
 const deleteProductById = async (req,res) => {
     try{
-        await UserSchema.findByIdAndDelete(req.params.id)
+        await ProductSchema.findByIdAndDelete(req.params.id)
 
         res.status(200).json({msg: `Product deleted`})
     }catch(e) {
